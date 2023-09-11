79f659e0874f401d332e07b662842d555924e30e
Methusela <nethusela777@gmail.com>
2023-09-11 21:40:34 +0200

    Initial commit

---
+++ src/main/java/org/example/MonthTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/*
+Exercise: Enumerations
+create an enumeration Month with all the name of the months
+iterate all the values of the enumeration, printing the name of the month followed by:
+ends with y if the name ends with Y
+doesn't end with y if the name doesn't end with y
+ */
+
